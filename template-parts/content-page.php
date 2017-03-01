ac882471531f3af96370d79f4335a2f5115cb94e
Hussain Abbas <habbas@keyhole.co>
2017-03-01 03:26:59 -0500

    default page layouts

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -19,30 +19,17 @@
 ?>
 
 <?php if (!$group) : ?>
-  <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-    <header class="entry-header">
-      <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-    </header><!-- .entry-header -->
+  <section class="hero">
+    <div class="wrapper">
+      <div class="hero__heading-container">
+        <?php the_title('<h1 class="hero__title">', '</h1>'); ?>
+      </div>
+    </div>
+  </section>
 
-    <div class="entry-content">
-
-      <?php if( get_field('hero_title') ): ?>
-        <h2><?php the_field('hero_title'); ?></h2>
-      <?php endif; ?>
-
-      <h3>Group: NO GROUP</h3>
-
+  <section class="content">
+    <div class="wrapper">
       <?php the_content(); ?>
-      <?php
-        wp_link_pages( array(
-          'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'yms' ),
-          'after'  => '</div>',
-        ) );
-      ?>
-    </div><!-- .entry-content -->
-
-    <footer class="entry-footer">
-      <?php edit_post_link( esc_html__( 'Edit', 'yms' ), '<span class="edit-link">', '</span>' ); ?>
-    </footer><!-- .entry-footer -->
-  </article><!-- #post-## -->
+    </div>
+  </section>
 <?php endif; ?>
